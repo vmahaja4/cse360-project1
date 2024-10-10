28057388b3ea4cdf1a3c99276ec07044e547213f
vmahaja4 <vmahaja4@asu.edu>
2024-10-09 22:53:27 -0700

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module cse360project1 {
+	requires javafx.graphics;
+	requires javafx.controls;
+	requires org.junit.jupiter.api;
+    requires javafx.fxml;
+    requires java.sql;
+    exports cse360project1 to javafx.graphics;
+
+}
